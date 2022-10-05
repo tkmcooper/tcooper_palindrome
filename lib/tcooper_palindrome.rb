aa654ab59748ef615fa45d6f5032bef908d6c088
Terry Cooper <coopertkm@gmail.com>
2022-10-06 00:12:26 +0100

    Initialise Repo

---
+++ lib/tcooper_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tcooper_palindrome/version"
+
+module TcooperPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tcooper_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TcooperPalindrome
+  VERSION = "0.1.0"
+end
